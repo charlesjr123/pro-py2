7a97025080adf4af3c1b6afa89fe90e4e972c574
happy monkey <70238658+charlesjr123@users.noreply.github.com>
2021-08-23 12:16:05 -0400

    Add files via upload

---
+++ New.py
@@ -0,0 +1,11 @@
+def count():
+    number_request=input("put in number request")
+    while number_request < 5:
+    if guess===number_request:
+    #if number by the same user is granted 
+    #number statement "break"
+    print("congrats you won")
+    print(number_request)
+
+    if not number_request <5:
+        print("You lose",number_request)
